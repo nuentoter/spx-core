ce7926448038ec300c8780a1f1177bfc134632fd
nuentoter <nuentoter@gmail.com>
2026-06-09 22:51:11 -0400

    Create wolf.js

---
+++ entities/wolf.js
@@ -0,0 +1,14 @@
+export function createWolf(x, y) {
+  return {
+    x,
+    y,
+    type: "wolf",
+    energy: 12,
+
+    genes: {
+      speed: 1,
+      vision: 8,
+      metabolism: 0.05
+    }
+  };
+}
