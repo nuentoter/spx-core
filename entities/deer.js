915e6a12520f896667593a07e17862a5543dc48e
nuentoter <nuentoter@gmail.com>
2026-06-09 22:51:59 -0400

    Create deer.js

---
+++ entities/deer.js
@@ -0,0 +1,14 @@
+export function createDeer(x, y) {
+  return {
+    x,
+    y,
+    type: "deer",
+    energy: 12,
+
+    genes: {
+      speed: 1,
+      vision: 6,
+      metabolism: 0.02
+    }
+  };
+}
