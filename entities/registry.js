6f683e27cbc9bc67a5d0ea222cb8bb788c8137b5
nuentoter <nuentoter@gmail.com>
2026-06-09 22:54:46 -0400

    Create registry.js

---
+++ entities/registry.js
@@ -0,0 +1,9 @@
+import { createWolf } from "./wolf.js";
+import { createDeer } from "./deer.js";
+import { createPlant } from "./plant.js";
+
+export {
+  createWolf,
+  createDeer,
+  createPlant
+};
