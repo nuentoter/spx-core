670409c3641d31a8841c7568b44a57f09454dda9
nuentoter <nuentoter@gmail.com>
2026-06-09 22:41:06 -0400

    Create world.js

---
+++ world/world.js
@@ -0,0 +1,25 @@
+import { createWolf, createDeer } from "../entities/species.js";
+
+export const World = {
+  width: 40,
+  height: 25,
+
+  entities: {
+    wolf_1: createWolf(5, 5),
+    wolf_2: createWolf(8, 6),
+
+    deer_1: createDeer(15, 10),
+    deer_2: createDeer(20, 12),
+    deer_3: createDeer(18, 14)
+  },
+
+  food: [],
+
+  removeDead() {
+    for (const id in this.entities) {
+      if (this.entities[id].energy <= 0) {
+        delete this.entities[id];
+      }
+    }
+  }
+};
