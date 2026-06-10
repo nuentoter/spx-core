1c2bf70fb61478c3b55577aa068b9f47761fd754
nuentoter <nuentoter@gmail.com>
2026-06-09 23:13:04 -0400

    Update world.js

--- world/world.js
+++ world/world.js
@@ -12,15 +12,19 @@ export const World = {
 
     deer_1: createDeer(15, 10),
     deer_2: createDeer(20, 12),
-    deer_3: createDeer(18, 14)
+    deer_3: createDeer(18, 14),
+    deer_4: createDeer(22, 8)
   },
 
   plants: {
     plant_1: createPlant(10, 10),
     plant_2: createPlant(18, 12),
     plant_3: createPlant(25, 14),
-    plant_4: createPlant(30, 8)
+    plant_4: createPlant(30, 8),
+    plant_5: createPlant(12, 16)
   },
+
+  tick: 0,
 
   removeDead() {
     for (const id in this.entities) {
